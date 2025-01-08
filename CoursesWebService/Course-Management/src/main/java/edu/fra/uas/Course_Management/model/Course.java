6c34432948839ab8cc733f737e25a655a3e35af8
ioanaxcarmen <ioanacarmen633@yahoo.com>
2025-01-08 21:04:53 +0100

    Einrichtung des Spring Boot Projekts
    Ein Spring Boot Projekt wurde für das Backend "Kursverwaltung" erstellt. Die notwendigen Pakete und Klassen Course und File wurden hinzugefügt.

---
+++ CoursesWebService/Course-Management/src/main/java/edu/fra/uas/Course_Management/model/Course.java
@@ -0,0 +1,45 @@
+package edu.fra.uas.Course_Management.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Course {
+    private long id;
+    private String name;
+    private String description;
+    private String instructor;
+    private List<File> files = new ArrayList<>();
+
+    // Getters and Setters
+    
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public String getInstructor() {
+        return instructor;
+    }
+    public void setInstructor(String instructor) {
+        this.instructor = instructor;
+    }
+    public List<File> getFiles() {
+        return files;
+    }
+    public void setFiles(List<File> files) {
+        this.files = files;
+    }
+}
---
+++ CoursesWebService/Course-Management/src/test/java/edu/fra/uas/Course_Management/CourseManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.fra.uas.Course_Management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
